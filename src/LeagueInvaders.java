b5603ca4b4a8dbf554f6a79f1eabbbb11a17d8b1
league <league@imac-20.attlocal.net>
2022-03-11 17:02:53 -0800

    that was interesting

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
-
+import javax.swing.JFrame;
 public class LeagueInvaders {
-
+JFrame frame;
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+	public static void main(String[]args) {
+	LeagueInvaders invader = new LeagueInvaders();
+	invader.setup();
+	}
+		public LeagueInvaders() {
+			frame = new JFrame();
+			
+		}
+		void setup() {
+			frame.setSize(WIDTH, HEIGHT);
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+		}
 }
